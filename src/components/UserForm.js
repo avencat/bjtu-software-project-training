39b768a2fcc1f4ef07ab3c2272dd2a9352c90032
Axel Vencatareddy <avencatareddy@me.com>
2017-10-30 02:38:14 +0800

    Added material icons font and updated UserForm to add icons to inputs

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -17,8 +17,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="login">Username :</label>
 
-          <div className="col-sm-8">
+          <div className="input-group col-sm-8">
             <input className="form-control" required={this.props.loginRequired} value={this.props.login} name='login' id="login" onChange={this.props.onChange}/>
+            <span className="input-group-addon"><i className="material-icons">account_circle</i></span>
           </div>
 
           <span className="col-sm-1"/>
@@ -29,8 +30,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="firstname">First name :</label>
 
-          <div className="col-sm-8">
+          <div className="input-group col-sm-8">
             <input className="form-control" value={this.props.firstname} name='firstname' id="firstname" onChange={this.props.onChange}/>
+            <span className="input-group-addon"><i className="material-icons">account_circle</i></span>
           </div>
 
           <span className="col-sm-1"/>
@@ -41,8 +43,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="lastname">Last name :</label>
 
-          <div className="col-sm-8">
+          <div className="input-group col-sm-8">
             <input className="form-control" value={this.props.lastname} name='lastname' id="lastname" onChange={this.props.onChange}/>
+            <span className="input-group-addon"><i className="material-icons">account_circle</i></span>
           </div>
 
           <span className="col-sm-1"/>
@@ -53,8 +56,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="email">Email :</label>
 
-          <div className="col-sm-8">
+          <div className="input-group col-sm-8">
             <input className="form-control" required={this.props.emailRequired} type="email" value={this.props.email} name='email' id="email" onChange={this.props.onChange}/>
+            <span className="input-group-addon"><i className="material-icons">@</i></span>
           </div>
 
           <span className="col-sm-1"/>
@@ -65,8 +69,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="password">Password :</label>
 
-          <div className="col-sm-8">
-            <input className="form-control" required={this.props.passwordRequired} type="password" value={this.props.password} name='password' id="password" onChange={this.props.onChange}/>
+          <div className="input-group col-sm-8">
+            <input className="form-control" required={this.props.passwordRequired} type="password" value={this.props.password} name='password' id="password" onChange={this.props.onChange} aria-describedby="passwordHelpBlock"/>
+            <span className="input-group-addon"><i className="material-icons">lock</i></span>
           </div>
 
           <span className="col-sm-1"/>
@@ -77,8 +82,9 @@ export default class UserForm extends Component {
 
           <label className="control-label col-sm-2" htmlFor="confirmedPassword">Confirm password :</label>
 
-          <div className="col-sm-8">
+          <div className="input-group col-sm-8">
             <input className="form-control" required={this.props.passwordRequired} type="password" value={this.props.confirmedPassword} name='confirmedPassword' id="confirmedPassword" onChange={this.props.onChange}/>
+            <span className="input-group-addon"><i className="material-icons">lock</i></span>
           </div>
 
           <span className="col-sm-1"/>

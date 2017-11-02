fbe01fbf63367fc263c74c2f5d2106fcc4be46bb
Axel Vencatareddy <avencatareddy@me.com>
2017-11-02 23:58:23 +0800

    Added DELETE to Access-Control-Allow-Methods

--- api/app.js
+++ api/app.js
@@ -160,7 +160,7 @@ app.use(session({
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
   next();
 });
 

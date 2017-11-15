97743ec9a7d2d25da02a8908f7e99c55e45ba672
Axel Vencatareddy <axel@koosmikcorp.com>
2017-11-16 03:28:47 +0800

    Added documentation and steps to install the project

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
 # bjtu-software-project-training
 We have to make a Social Network and use PostgreSQL, we will make the app with React.JS and Bootstrap for the front-end and Node.JS, express, passport and pg-promise for the back-end.
+
+## Prerequesities
+You must have some dependencies in order to run the program:
+- `npm` or `yarn` (as you wish) with `node`
+- PostgreSQL ≥ 9.6 with `psql` installed
+
+## Installation
+### Follow these steps (inside a terminal):
+1. Clone the repo:
+    ```
+    git clone https://github.com/avencat/bjtu-software-project-training.git
+    ```
+2. Go to the repo:
+    ```
+    cd bjtu-software-project-training
+    ```
+3. Install dependencies for the front-end:
+    ```
+    yarn
+    ```
+    or
+    ```
+    npm install
+    ```
+4. Go to the API dir:
+    ```
+    cd api
+    ```
+5. Install dependencies for the API:
+    ```
+    yarn
+    ```
+    or
+    ```
+    npm install
+    ```
+
+## Configuration
+The default configuration that comes with the project is as follow:
+
+- Database name: `socialnetwork`
+- Test database name: `socialnetworktest`
+- PostgreSQL user to connect to the databases: `postgres`
+- Password of the user: `root`
+
+You can let the default configuration and go straight to the next part or modify that configuration with these simple steps:
+
+- Modify the `api/config.json` file and replace the end of the two urls with the names of the databases you want to use.
+- Modify the first three lines of the files `api/database.sql` and `api/database-test.sql` by replacing the names of the databases with those that you want to use (They MUST match the names that you entered in the file `api/config.json` at the previous step).
+- Don't forget to enter in `api/config.json` the credentials of a PostgreSQL user that has the rights to CREATE a database
+
+You file `config.json` should look like something like this:
+```json
+{
+  "database" : {
+    "test": "postgres://user:password@localhost:5432/socialnetworktest",
+    "dev": "postgres://user:password@localhost:5432/socialnetwork"
+  }
+}
+```
+where user is a PostgreSQL user with the CREATE DATABASE right and password this user's password.
+
+## Database installation
+### In a terminal, go inside the API directory and type:
+```
+psql -f database.sql -u postgres -d postgres
+psql -f database-test.sql -u postgres -d postgres
+```
+
+## Run the tests (Strongly advised)
+After the installation of the databases with the two `psql` commands, you might want to run the test in order to see if the project were correctly installed.
+
+In the API directory launch:
+```
+yarn test
+```
+or
+```
+npm test
+```
+
+## Run the program
+In the API directory:
+```
+yarn start
+```
+or
+```
+npm start
+```
+
+Then open a new terminal, go to the root project directory and launch the web server:
+```
+yarn start
+```
+or
+```
+npm start
+```
+
+Then go to `http://localhost:3001/register` and you should be able to register you first user.
+
+#### Congratulations!
+
+### Bonus: API documentation
+You might want to call the API directly. We provided a documentation in order to let you know what you can do with our API. You can open it in your browser. It's located in `api/doc/index.html`
---
+++ api/database-test.sql
@@ -0,0 +1,356 @@
+DROP DATABASE IF EXISTS socialnetworktest;
+CREATE DATABASE socialnetworktest;
+
+\c socialnetworktest;
+
+CREATE TABLE      users (
+  id              SERIAL PRIMARY KEY,
+  firstname       TEXT,
+  lastname        TEXT,
+  birthday        DATE,
+  email           TEXT UNIQUE NOT NULL,
+  login           TEXT UNIQUE NOT NULL,
+  gender          BIGINT,
+  telephone       TEXT UNIQUE,
+  password        TEXT NOT NULL,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  following_nb    BIGINT NOT NULL DEFAULT 0,
+  follower_nb     BIGINT NOT NULL DEFAULT 0,
+  FOREIGN KEY     (gender) REFERENCES gender(id),
+  CONSTRAINT      login_min_length CHECK (length(login) >= 5)
+);
+
+CREATE INDEX      fullname ON users (firstname, lastname);
+
+CREATE TABLE      friendships (
+  id              SERIAL PRIMARY KEY,
+  follower_id     BIGINT NOT NULL,
+  following_id    BIGINT NOT NULL,
+  following_date  TIMESTAMPTZ,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  FOREIGN KEY     (follower_id)   REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY     (following_id)  REFERENCES users(id) ON DELETE CASCADE,
+  UNIQUE          (follower_id, following_id)
+);
+
+CREATE TABLE      posts (
+  id              SERIAL PRIMARY KEY,
+  author_id       BIGINT NOT NULL,
+  content         TEXT,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  comments_nb     BIGINT NOT NULL DEFAULT 0,
+  likes_nb        BIGINT NOT NULL DEFAULT 0,
+  FOREIGN KEY     (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE      comments (
+  id              SERIAL PRIMARY KEY,
+  author_id       BIGINT NOT NULL,
+  post_id         BIGINT NOT NULL,
+  content         TEXT,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  likes_nb        BIGINT NOT NULL DEFAULT 0,
+  FOREIGN KEY     (author_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY     (post_id)   REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE      post_likes (
+  id              SERIAL PRIMARY KEY,
+  user_id         BIGINT NOT NULL,
+  post_id         BIGINT NOT NULL,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  UNIQUE          (user_id, post_id),
+  FOREIGN KEY     (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY     (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE      comment_likes (
+  id              SERIAL PRIMARY KEY,
+  user_id         BIGINT NOT NULL,
+  comment_id      BIGINT NOT NULL,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ,
+  UNIQUE          (user_id, comment_id),
+  FOREIGN KEY     (user_id)     REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY     (comment_id)  REFERENCES comments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE      gender (
+  id              SERIAL PRIMARY KEY,
+  title           TEXT NOT NULL,
+  description     TEXT,
+  created         TIMESTAMPTZ,
+  updated         TIMESTAMPTZ
+);
+
+INSERT INTO       gender(title, description) VALUES('Male', 'Gender for boys and men');
+INSERT INTO       gender(title, description) VALUES('Female', 'Gender for girls and women');
+INSERT INTO       gender(title, description) VALUES('Other', 'Gender for everyone else');
+
+
+
+-- **************************
+-- * Functions and triggers *
+-- **************************
+
+CREATE FUNCTION manage_following_nb()
+  RETURNS TRIGGER AS $trigger_manage_following_nb$
+
+BEGIN
+
+  -- Check that follower_id is given
+  IF (TG_OP = 'INSERT') THEN
+    IF NEW.follower_id IS NULL THEN
+      RAISE EXCEPTION 'follower_id cannot be null';
+    ELSEIF NEW.following_id IS NULL THEN
+      RAISE EXCEPTION 'following_id cannot be null';
+    END IF;
+  END IF;
+
+  -- Substract or add one to following_nb
+  IF (TG_OP = 'DELETE') THEN
+
+    UPDATE users SET following_nb = following_nb - 1 WHERE id = OLD.follower_id;
+    UPDATE users SET follower_nb = follower_nb - 1 WHERE id = OLD.following_id;
+
+  ELSE
+
+    UPDATE users SET following_nb = following_nb + 1 WHERE id = NEW.follower_id;
+    UPDATE users SET follower_nb = follower_nb + 1 WHERE id = NEW.following_id;
+
+  END IF;
+
+  -- Fill the time fields
+  IF (TG_OP = 'INSERT') THEN
+
+    NEW.created := current_timestamp;
+    NEW.updated := current_timestamp;
+
+    RETURN NEW;
+
+  ELSE
+
+    RETURN OLD;
+
+  END IF;
+
+END;
+
+$trigger_manage_following_nb$ LANGUAGE plpgsql;
+
+CREATE FUNCTION manage_comments_nb()
+  RETURNS TRIGGER AS $trigger_manage_comments_nb$
+
+  BEGIN
+
+    -- Check that post_id is given
+    IF (TG_OP = 'INSERT') THEN
+      IF NEW.post_id IS NULL THEN
+        RAISE EXCEPTION 'post_id cannot be null';
+      END IF;
+    END IF;
+
+    -- Substract or add one to comments_nb
+    IF (TG_OP = 'DELETE') THEN
+      UPDATE posts SET comments_nb = comments_nb - 1 WHERE id = OLD.post_id;
+    ELSE
+      UPDATE posts SET comments_nb = comments_nb + 1 WHERE id = NEW.post_id;
+    END IF;
+
+    -- Fill the time fields
+    IF (TG_OP = 'INSERT') THEN
+      NEW.created := current_timestamp;
+      NEW.updated := current_timestamp;
+
+      RETURN NEW;
+    ELSE
+      RETURN OLD;
+    END IF;
+
+  END;
+
+$trigger_manage_comments_nb$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION manage_likes_nb()
+  RETURNS TRIGGER AS $trigger_manage_likes_nb$
+
+  BEGIN
+
+    -- Check that post_id is given
+    IF (TG_OP = 'INSERT') THEN
+      IF NEW.post_id IS NULL THEN
+        RAISE EXCEPTION 'post_id cannot be null';
+      END IF;
+    END IF;
+
+    -- Substract or add one to likes_nb
+    IF (TG_OP = 'DELETE') THEN
+      UPDATE posts SET likes_nb = posts.likes_nb - 1 WHERE id = OLD.post_id;
+    ELSE
+      UPDATE posts SET likes_nb = posts.likes_nb + 1 WHERE id = NEW.post_id;
+    END IF;
+
+    -- Fill the time fields
+    IF (TG_OP = 'INSERT') THEN
+      NEW.created := current_timestamp;
+      NEW.updated := current_timestamp;
+
+      RETURN NEW;
+    ELSE
+      RETURN OLD;
+    END IF;
+
+  END;
+
+$trigger_manage_likes_nb$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION manage_comment_likes_nb()
+  RETURNS TRIGGER AS $trigger_manage_comment_likes_nb$
+
+  BEGIN
+
+    -- Check that post_id is given
+    IF (TG_OP = 'INSERT') THEN
+      IF NEW.comment_id IS NULL THEN
+        RAISE EXCEPTION 'post_id cannot be null';
+      END IF;
+    END IF;
+
+    -- Substract or add one to likes_nb
+    IF (TG_OP = 'DELETE') THEN
+      UPDATE comments SET likes_nb = comments.likes_nb - 1 WHERE id = OLD.comment_id;
+    ELSE
+      UPDATE comments SET likes_nb = comments.likes_nb + 1 WHERE id = NEW.comment_id;
+    END IF;
+
+    -- Fill the time fields
+    IF (TG_OP = 'INSERT') THEN
+      NEW.created := current_timestamp;
+      NEW.updated := current_timestamp;
+
+      RETURN NEW;
+    ELSE
+      RETURN OLD;
+    END IF;
+
+  END;
+
+$trigger_manage_comment_likes_nb$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION sanitize_username_and_email()
+  RETURNS TRIGGER AS $trigger_sanitize_username_and_email$
+
+BEGIN
+
+  -- trim and lower email and login
+  IF NEW.email IS NOT NULL THEN
+    NEW.email := lower(trim(NEW.email));
+  END IF;
+
+  IF NEW.login IS NOT NULL THEN
+    NEW.login := lower(trim(NEW.login));
+  END IF;
+
+  -- Fill the time fields
+  IF (TG_OP = 'INSERT') THEN
+    NEW.created := current_timestamp;
+  END IF;
+  NEW.updated := current_timestamp;
+
+  RETURN NEW;
+
+END
+
+$trigger_sanitize_username_and_email$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION manage_posts()
+  RETURNS TRIGGER AS $trigger_manage_posts$
+
+BEGIN
+
+  -- Check that author_id is given
+  IF NEW.author_id IS NULL THEN
+    RAISE EXCEPTION 'author_id cannot be null.';
+  END IF;
+
+  -- Fill the time fields
+  IF (TG_OP = 'INSERT') THEN
+    NEW.created := current_timestamp;
+  END IF;
+  NEW.updated := current_timestamp;
+
+  RETURN NEW;
+
+END
+
+$trigger_manage_posts$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION manage_comments()
+  RETURNS TRIGGER AS $trigger_manage_comments$
+
+BEGIN
+
+  -- Check that author_id && post_id are given
+  IF NEW.author_id IS NULL THEN
+    RAISE EXCEPTION 'author_id cannot be null.';
+  ELSEIF NEW.post_id IS NULL THEN
+    RAISE EXCEPTION 'post_id cannot be null.';
+  END IF;
+
+  -- Fill the time fields
+  IF (TG_OP = 'INSERT') THEN
+    NEW.created := current_timestamp;
+  END IF;
+  NEW.updated := current_timestamp;
+
+  RETURN NEW;
+
+END
+
+$trigger_manage_comments$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trigger_manage_posts
+  BEFORE INSERT OR UPDATE ON posts
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_posts();
+
+CREATE TRIGGER trigger_manage_comments
+  BEFORE INSERT OR UPDATE ON comments
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_comments();
+
+CREATE TRIGGER trigger_manage_comments_nb
+  AFTER INSERT OR DELETE ON comments
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_comments_nb();
+
+CREATE TRIGGER trigger_manage_following_nb
+  AFTER INSERT OR DELETE ON friendships
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_following_nb();
+
+CREATE TRIGGER trigger_manage_likes_nb
+  AFTER INSERT OR DELETE ON post_likes
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_likes_nb();
+
+CREATE TRIGGER trigger_manage_comment_likes_nb
+  AFTER INSERT OR DELETE ON comment_likes
+  FOR EACH ROW
+  EXECUTE PROCEDURE manage_comment_likes_nb();
+
+CREATE TRIGGER trigger_sanitize_username_and_email
+  BEFORE INSERT OR UPDATE ON users
+  FOR EACH ROW
+  EXECUTE PROCEDURE sanitize_username_and_email();

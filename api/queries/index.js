1b3ea4a018604c20148145b0fecd0912d6077607
Axel Vencatareddy <axel@koosmikcorp.com>
2017-11-26 02:37:43 +0800

    Return count instead of an array for followingNb and followerNb

--- api/queries/index.js
+++ api/queries/index.js
@@ -6,14 +6,14 @@ function getFollowingNb(req, res, next) {
 
   const user_id = (req.query.user_id || req.query.author_id) ? parseInt(req.query.user_id | req.query.author_id) : req.user.id;
 
-  db.any(format('SELECT COUNT(id) FROM friendships WHERE follower_id = %L', user_id))
+  db.one(format('SELECT COUNT(id) FROM friendships WHERE follower_id = %L', user_id))
 
     .then((data) => {
 
       res.status(200)
         .json({
           status: 'success',
-          data,
+          data: data.count,
           message: 'Retrieved number of users that follow user ' + user_id
         });
 
@@ -30,14 +30,14 @@ function getFollowerNb(req, res, next) {
 
   const user_id = (req.query.user_id || req.query.author_id) ? parseInt(req.query.user_id | req.query.author_id) : req.user.id;
 
-  db.any(format('SELECT COUNT(id) FROM friendships WHERE following_id = %L', user_id))
+  db.one(format('SELECT COUNT(id) FROM friendships WHERE following_id = %L', user_id))
 
     .then((data) => {
 
       res.status(200)
         .json({
           status: 'success',
-          data,
+          data: data.count,
           message: 'Retrieved number of users that user ' + user_id + ' is following'
         });
 
